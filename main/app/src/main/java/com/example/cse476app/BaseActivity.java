d3e1eb5c2faac5b5644dd98710ad3c32907a6b24
AaronElk4124 <aaronelkin0@gmail.com>
2024-11-10 17:08:30 -0500

    Basic Authentication set up

--- main/app/src/main/java/com/example/cse476app/BaseActivity.java
+++ main/app/src/main/java/com/example/cse476app/BaseActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 /*
     * BaseActivity is an abstract class that extends AppCompatActivity and provides a common
@@ -23,9 +25,20 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_base);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId("1:267773612973:android:ca81d5d09a43b168064724")
+                .setApiKey("AIzaSyBfy1QBurX2hpq5abTak_Dg59SjgEro_CQ") // Web API Key from your screenshot
+                .setProjectId("cse476-4ad07")                      // Project ID from your screenshot
+                .setDatabaseUrl("https://cse476-4ad07.firebaseio.com") // Optional if using Realtime Database
+                .build();
+
+        // Check if FirebaseApp is already initialized to avoid duplicate initialization
+        if (FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseApp.initializeApp(this, options);
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.base), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
